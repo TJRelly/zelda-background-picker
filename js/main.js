09fea02f04b10cb226a338aaa07590bf7911bf58
TJ <xxterrelljacksonxx@gmail.com>
2023-02-22 12:01:58 -0500

    css update

--- js/main.js
+++ js/main.js
@@ -7,21 +7,22 @@ document.getElementById('view').onclick = viewPic
 function sword2Pic() {
   document.querySelector('body').style.background = "url('./img/link-sword-2.jpg') center"
   document.querySelector('body').style.backgroundSize = 'cover'
-  document.querySelector('h1').style.background = 'rgba(128,128,128, 0.8)'
+  document.querySelector('h1').style.background = 'rgba(128,128,128, 0.5)'
+  document.querySelector('h1').style.color = 'white'
 }
 
 function arrowPic() {
   document.querySelector('body').style.background = "url('./img/link-arrow.jpg') center"
   document.querySelector('body').style.backgroundSize = 'cover'
   document.querySelector('h1').style.color = 'gold'
-  document.querySelector('h1').style.background = 'rgba(128,128,128, 0.8)'
+  document.querySelector('h1').style.background = 'rgba(128,128,128, 0.5)'
 }
 
 function horsePic() {
   document.querySelector('body').style.background = "url('./img/link-horse.jpg') center"
   document.querySelector('body').style.backgroundSize = 'cover'
   document.querySelector('h1').style.color = 'lightgreen'
-  document.querySelector('h1').style.background = 'rgba(128,128,128, 0.8)'
+  document.querySelector('h1').style.background = 'rgba(128,128,128, 0.5)'
 }
 
 
@@ -29,14 +30,14 @@ function swordPic() {
   document.querySelector('body').style.background = "url('./img/link-sword.jpg') center"
   document.querySelector('body').style.backgroundSize = 'cover'
   document.querySelector('h1').style.color = 'gold'
-  document.querySelector('h1').style.background = 'rgba(128,128,128, 0.8)'
+  document.querySelector('h1').style.background = 'rgba(128,128,128, 0.5)'
 }
 
 function viewPic() {
   document.querySelector('body').style.background = "url('./img/link-view.jpg') center"
   document.querySelector('body').style.backgroundSize = 'cover'
   document.querySelector('h1').style.color = 'skyblue'
-  document.querySelector('h1').style.background = 'rgba(128,128,128, 0.8)'
+  document.querySelector('h1').style.background = 'rgba(128,128,128, 0.5)'
 }
 
 
